044de73f00fed68073e6eee4d6147cbb9719425a
Aris Tritas <aris.tritas@aiven.io>
2024-12-03 13:31:00 +0100

    Add Backup disk type
    This adds a new disk type that wraps an object storage disk and
    enables coordination with our backup system.
    
    When this disk type is used as a wrapper on top of an object storage
    disk, the files in object storage are not really deleted.
    
    Instead, a local file is created for each file in object storage that
    should be deleted.
    
    Our backup system will then be in charge to do garbage collection
    and only delete files are are not referenced by any backup.
    
    The file names of the deletion markers are like this:
    CLICKHOUSE_DIR/disks/remote_backup/backup/ESCAPED_OBJECT_PATH
    
    The object path is escaped to simplify directory management and uses
    the same escape mechanism that ClickHouse uses in many places to
    reference files (essentially escape encoding).
    
    In case Backup disk is wrapping a Cache disk then it lets the remove
    calls pass
    
    This commit was applied from the patch file 0059-Backup_disk.patch
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Disks/ObjectStorages/DiskObjectStorage.h
+++ src/Disks/ObjectStorages/DiskObjectStorage.h
@@ -195,14 +195,19 @@ public:
     /// DiskObjectStorage(CachedObjectStorage(CachedObjectStorage(S3ObjectStorage)))
     String getStructure() const { return fmt::format("DiskObjectStorage-{}({})", getName(), object_storage->getName()); }
 
+    // Add a backup layer
+    void wrapWithBackup(const String & layer_name, const String & backup_base_path);
+
     /// Add a cache layer.
     /// Example: DiskObjectStorage(S3ObjectStorage) -> DiskObjectStorage(CachedObjectStorage(S3ObjectStorage))
     /// There can be any number of cache layers:
     /// DiskObjectStorage(CachedObjectStorage(...CacheObjectStorage(S3ObjectStorage)...))
     void wrapWithCache(FileCachePtr cache, const FileCacheSettings & cache_settings, const String & layer_name);
 
-    /// Get names of all cache layers. Name is how cache is defined in configuration file.
-    NameSet getCacheLayersNames() const override;
+    bool supportsLayers() const override { return true; }
+
+    /// Get names of all backup and cache layers. Name is how backup/cache is defined in configuration file.
+    NameSet getLayersNames() const override;
 
     bool supportsStat() const override { return metadata_storage->supportsStat(); }
     struct stat stat(const String & path) const override;
