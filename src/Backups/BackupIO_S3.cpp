cd4a127f6efc0e70a205ce53ecdfe6c3fa3a8469
Aris Tritas <aris.tritas@aiven.io>
2024-12-03 13:30:49 +0100

    Allow custom CA in S3
    For testing purposes, we want to be able to connect to an object storage
    server that does not have a certificate signed by a public authority,
    while still testing it with HTTPS.
    
    ClickHouse does not have that option, but the underlying library
    (POCO), supports it, just pass the parameter around, from the disk
    settings to the library.
    
    This commit was applied from the patch file 0047-Custom_S3_certificate_authority.patch
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Backups/BackupIO_S3.cpp
+++ src/Backups/BackupIO_S3.cpp
@@ -90,6 +90,7 @@ namespace
             static_cast<unsigned>(local_settings[Setting::s3_max_redirects]),
             static_cast<unsigned>(local_settings[Setting::backup_restore_s3_retry_attempts]),
             local_settings[Setting::enable_s3_requests_logging],
+            /* ca_path */ std::optional<String>(),
             /* for_disk_s3 = */ false,
             request_settings.get_request_throttler,
             request_settings.put_request_throttler,
