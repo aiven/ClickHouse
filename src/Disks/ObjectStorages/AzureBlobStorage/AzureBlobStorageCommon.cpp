545f3e305d4b37c2480271222e7ae7c8ff2c1ecf
Aris Tritas <aris.tritas@aiven.io>
2024-12-04 11:27:03 +0100

    Allow delegating Azure signature to a separate process
    The process is accessed over HTTP and the URL to access it can be
    configured with the signature_delegation_url parameter in the Azure
    disk configuration.
    
    ClickHouse will make a POST request with a json body:
    {
        "stringToSing": "..."
    }
    And expects a JSON response with status 200 and this structure:
    {
        "signature": 01234567890abcdefg..."
    }
    
    The string to sign matches the format defined by Azure for
    signatures.
    
    The canonical request also contains all the required  information
    (path, operation...) to let the proxy decide of the request is
    allowed and can be signed. It's also enough information to know
    the cost of the request.
    
    This commit was applied from the patch file 0067-Delegated_signature_azure.patch
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Disks/ObjectStorages/AzureBlobStorage/AzureBlobStorageCommon.cpp
+++ src/Disks/ObjectStorages/AzureBlobStorage/AzureBlobStorageCommon.cpp
@@ -1,4 +1,5 @@
 #include <Disks/ObjectStorages/AzureBlobStorage/AzureBlobStorageCommon.h>
+#include <Disks/ObjectStorages/AzureBlobStorage/AzureDelegatedKeyPolicy.h>
 
 #if USE_AZURE_BLOB_STORAGE
 
@@ -164,6 +165,10 @@ std::unique_ptr<ContainerClient> ConnectionParams::createForContainer() const
         if constexpr (std::is_same_v<T, ConnectionString>)
         {
             auto raw_client = RawContainerClient::CreateFromConnectionString(auth.toUnderType(), endpoint.container_name, client_options);
+            return std::make_unique<ContainerClient>(std::move(raw_client), endpoint.prefix);
+        }
+        else if (delegated_signature) {
+            RawContainerClient raw_client{endpoint.getContainerEndpoint(), client_options};
             return std::make_unique<ContainerClient>(std::move(raw_client), endpoint.prefix);
         }
         else
@@ -387,6 +392,10 @@ AuthMethod getAuthMethod(const Poco::Util::AbstractConfiguration & config, const
     return std::make_shared<Azure::Identity::ManagedIdentityCredential>();
 }
 
+bool isDelegatedSignature(const RequestSettings &settings) {
+    return settings.account_name.has_value() && settings.signature_delegation_url.has_value();
+}
+
 BlobClientOptions getClientOptions(const RequestSettings & settings, bool for_disk)
 {
     Azure::Core::Http::Policies::RetryOptions retry_options;
@@ -401,6 +410,14 @@ BlobClientOptions getClientOptions(const RequestSettings & settings, bool for_di
         curl_options.CAInfo = settings.curl_ca_path.value();
 
     Azure::Storage::Blobs::BlobClientOptions client_options;
+    if (settings.account_name.has_value() && settings.signature_delegation_url.has_value())
+    {
+        auto storage_shared_key_credential
+            = std::make_shared<Azure::Storage::StorageSharedKeyCredential>(settings.account_name.value(), /* account_key= */ "ignored");
+        client_options.PerRetryPolicies.emplace_back(
+            std::make_unique<AzureDelegatedKeyPolicy>(storage_shared_key_credential, settings.signature_delegation_url.value()));
+    }
+
     client_options.Retry = retry_options;
     client_options.Transport.Transport = std::make_shared<Azure::Core::Http::CurlTransport>(curl_options);
     client_options.ClickhouseOptions = Azure::Storage::Blobs::ClickhouseClientOptions{.IsClientForDisk=for_disk};
@@ -484,6 +501,11 @@ std::unique_ptr<RequestSettings> getRequestSettings(const Poco::Util::AbstractCo
     if (config.has(config_prefix + ".ca_path"))
     {
         settings->curl_ca_path = config.getString(config_prefix + ".ca_path");
+    }
+
+    if (config.has(config_prefix + ".account_name") && config.has(config_prefix + ".signature_delegation_url")) {
+        settings->account_name = config.getString(config_prefix + ".account_name");
+        settings->signature_delegation_url = config.getString(config_prefix + ".signature_delegation_url");
     }
 
     return settings;
