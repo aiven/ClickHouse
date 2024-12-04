f6323d7c70cd649a9c44e35b8628c74b60fe219b
Aris Tritas <aris.tritas@aiven.io>
2024-12-04 11:09:38 +0100

    Add kafka_auto_reset_mode and kafka_date_time_input_format settings
    kafka_auto_reset_mode was only configurable by topic through the global
    config file and kafka_date_time_input_format was only configurable through
    the global config file or the context (but context settings affecting
    CREATE query do not work well with replication/restoration).
    
    This commit was applied from the patch file 0062-Kafka_extra_settings.patch
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Storages/Kafka/KafkaSettings.cpp
+++ src/Storages/Kafka/KafkaSettings.cpp
@@ -26,6 +26,7 @@ namespace ErrorCodes
     /* those are mapped to format factory settings */ \
     DECLARE(String, kafka_format, "", "The message format for Kafka engine.", 0) \
     DECLARE(String, kafka_format_avro_schema_registry_url, "", "For AvroConfluent format: Kafka Schema Registry URL.", 0) \
+    DECLARE(DateTimeInputFormat, kafka_date_time_input_format, FormatSettings::DateTimeInputFormat::Basic, "Method to read DateTime from text input formats. Possible values: 'basic', 'best_effort' and 'best_effort_us'.", 0) \
     DECLARE(String, kafka_schema, "", "Schema identifier (used by schema-based formats) for Kafka engine", 0) \
     DECLARE(UInt64, kafka_num_consumers, 1, "The number of consumers per table for Kafka engine.", 0) \
     /* default is = max_insert_block_size / kafka_num_consumers  */ \
@@ -41,6 +42,7 @@ namespace ErrorCodes
     DECLARE(Milliseconds, kafka_flush_interval_ms, 0, "Timeout for flushing data from Kafka.", 0) \
     DECLARE(Bool, kafka_thread_per_consumer, false, "Provide independent thread for each consumer", 0) \
     DECLARE(StreamingHandleErrorMode, kafka_handle_error_mode, StreamingHandleErrorMode::DEFAULT, "How to handle errors for Kafka engine. Possible values: default (throw an exception after kafka_skip_broken_messages broken messages), stream (save broken messages and errors in virtual columns _raw_message, _error).", 0) \
+    DECLARE(KafkaAutoOffsetReset, kafka_auto_offset_reset, KafkaAutoOffsetReset::EARLIEST, "Action to take when there is no initial offset in offset store or the desired offset is out of range. Possible values: smallest, earliest (default), beginning, largest, latest, end.", 0) \
     DECLARE(Bool, kafka_commit_on_select, false, "Commit messages when select query is made", 0) \
     DECLARE(UInt64, kafka_max_rows_per_message, 1, "The maximum number of rows produced in one kafka message for row-based formats.", 0) \
     /* Authentication settings */ \
