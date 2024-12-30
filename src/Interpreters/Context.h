8ff9d3cb7ad19d4d2e2d3d0eb9b48b1b8b7c8789
Aliaksei Khatskevich <alex.khatskevich@aiven.io>
2024-12-30 15:27:31 +0100

    Allow avnadmin creating database using sql
    There are 2 main changes:
    1. Allow `avnadmin` calling `CREATE DATABASE` and `DROP DATABASE `
    , which overrides during execution phase, ensuring correct parameters
    are passed and running it with `aiven` privileges.
    2. Introduce `GRANT DEFAULT REPLICATED DATABASE PRIVILEGES` statement,
    which will give a user a default set of privileges for a database.
    This is necessary to maintain a common source of truth while creating
    databases from different environments.
    
    [DDB-1615]
    [DDB-1839]
    [DDB-1968]

--- src/Interpreters/Context.h
+++ src/Interpreters/Context.h
@@ -688,6 +688,8 @@ public:
     void setUser(const UUID & user_id_, const std::vector<UUID> & external_roles_ = {});
     UserPtr getUser() const;
 
+    void setGlobalContext();
+
     std::optional<UUID> getUserID() const;
     String getUserName() const;
 
