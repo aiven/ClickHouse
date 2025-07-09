a9c3a7c1290d0e5a26edd4b78c1633073f1d83bf
Aliaksei Khatskevich <alex.khatskevich@aiven.io>
2025-07-10 00:11:22 +0200

    Add replicated_database_logs_to_keep server setting
    This setting allows changing default number of replicated
    database logs stored in zookeeper.
    Default value is changed from 1000 to 300 to greatly reduce
    zookeeper resource consumption for setups with many databases.

--- src/Databases/DatabaseReplicated.cpp
+++ src/Databases/DatabaseReplicated.cpp
@@ -554,7 +554,8 @@ bool DatabaseReplicated::createDatabaseNodesInZooKeeper(const zkutil::ZooKeeperP
     ops.emplace_back(zkutil::makeRemoveRequest(zookeeper_path + "/counter/cnt-", -1));
     ops.emplace_back(zkutil::makeCreateRequest(zookeeper_path + "/metadata", "", zkutil::CreateMode::Persistent));
     ops.emplace_back(zkutil::makeCreateRequest(zookeeper_path + "/max_log_ptr", "1", zkutil::CreateMode::Persistent));
-    ops.emplace_back(zkutil::makeCreateRequest(zookeeper_path + "/logs_to_keep", "1000", zkutil::CreateMode::Persistent));
+    auto logs_to_keep = getContext()->getServerSettings().replicated_database_logs_to_keep;
+    ops.emplace_back(zkutil::makeCreateRequest(zookeeper_path + "/logs_to_keep", std::to_string(logs_to_keep), zkutil::CreateMode::Persistent));
 
     Coordination::Responses responses;
     auto res = current_zookeeper->tryMulti(ops, responses);
