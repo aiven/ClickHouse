e8f2d7ebeb1817a3b1c5ed2602d34f83b21e0adf
Aliaksei Khatskevich <alex.khatskevich@aiven.io>
2025-10-22 23:01:02 +0200

    Fix: stop wiping secrets from remote calls
    Remote calls (like `azureBlobStorageCluster`) format `AST` into
    `String` using `formatWithSecretsOneLine`. This function wipes
    sensitive data, which can lead to incorrect remote call.
    
    This commit makes wiping optional and stops it during the
    remote calls.

--- src/Parsers/IAST.cpp
+++ src/Parsers/IAST.cpp
@@ -176,7 +176,8 @@ String IAST::formatWithPossiblyHidingSensitiveData(
     bool show_secrets,
     bool print_pretty_type_names,
     IdentifierQuotingRule identifier_quoting_rule,
-    IdentifierQuotingStyle identifier_quoting_style) const
+    IdentifierQuotingStyle identifier_quoting_style,
+    bool wipe_sensitive_data) const
 {
     WriteBufferFromOwnString buf;
     FormatSettings settings(one_line);
@@ -185,7 +186,9 @@ String IAST::formatWithPossiblyHidingSensitiveData(
     settings.identifier_quoting_rule = identifier_quoting_rule;
     settings.identifier_quoting_style = identifier_quoting_style;
     format(buf, settings);
-    return wipeSensitiveDataAndCutToLength(buf.str(), max_length);
+    if (wipe_sensitive_data)
+        wipeSensitiveDataAndCutToLength(buf.str(), max_length);
+    return buf.str();
 }
 
 String IAST::formatForLogging(size_t max_length) const
@@ -210,7 +213,7 @@ String IAST::formatForErrorMessage() const
         /*identifier_quoting_style=*/IdentifierQuotingStyle::Backticks);
 }
 
-String IAST::formatWithSecretsOneLine() const
+String IAST::formatWithSecretsOneLine(bool wipe_sensitive_data) const
 {
     return formatWithPossiblyHidingSensitiveData(
         /*max_length=*/0,
@@ -218,7 +221,8 @@ String IAST::formatWithSecretsOneLine() const
         /*show_secrets=*/true,
         /*print_pretty_type_names=*/false,
         /*identifier_quoting_rule=*/IdentifierQuotingRule::WhenNecessary,
-        /*identifier_quoting_style=*/IdentifierQuotingStyle::Backticks);
+        /*identifier_quoting_style=*/IdentifierQuotingStyle::Backticks,
+        /*wipe_sensitive_data=*/wipe_sensitive_data);
 }
 
 String IAST::formatWithSecretsMultiLine() const
