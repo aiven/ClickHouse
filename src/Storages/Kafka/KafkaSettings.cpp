2ff36f9ba8c0f5f479723e66551951e50e238585
Tilman Moeller <tilman.moeller@aiven.io>
2025-12-22 13:50:49 +0100

    Add Kafka configuration support for SASL and SSL settings
    This commit introduces new enums for Kafka SASL mechanisms, security protocols, and SSL endpoint identification algorithms. It also implements case-insensitive parsing for these settings to ensure backward compatibility. The KafkaConfigLoader is updated to handle these new configurations, allowing for more flexible and secure Kafka connections.
    
    Changes include:
    - Added `KafkaSASLMechanism`, `KafkaSecurityProtocol`, and `KafkaSSLEndpointIdentificationAlgorithm` enums.
    - Implemented `toString` and `fromString` methods for these enums with case-insensitive support for backwards compability.
    - Updated KafkaConfigLoader to apply security and SSL settings from the configuration parameters.
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Storages/Kafka/KafkaSettings.cpp
+++ src/Storages/Kafka/KafkaSettings.cpp
@@ -43,10 +43,14 @@ namespace ErrorCodes
     DECLARE(UInt64, kafka_max_rows_per_message, 1, "The maximum number of rows produced in one kafka message for row-based formats.", 0) \
     DECLARE(String, kafka_keeper_path, "", "The path to the table in ClickHouse Keeper", 0) \
     DECLARE(String, kafka_replica_name, "", "The replica name in ClickHouse Keeper", 0) \
-    DECLARE(String, kafka_security_protocol, "", "Protocol used to communicate with brokers.", 0) \
-    DECLARE(String, kafka_sasl_mechanism, "", "SASL mechanism to use for authentication. Supported: GSSAPI, PLAIN, SCRAM-SHA-256, SCRAM-SHA-512, OAUTHBEARER.", 0) \
+    DECLARE(KafkaSecurityProtocol, kafka_security_protocol, KafkaSecurityProtocol::PLAINTEXT, "Protocol used to communicate with brokers. Possible values: PLAINTEXT, SSL, SASL_PLAINTEXT, SASL_SSL.", 0) \
+    DECLARE(KafkaSASLMechanism, kafka_sasl_mechanism, KafkaSASLMechanism::GSSAPI, "SASL mechanism to use for authentication. Supported: GSSAPI, PLAIN, SCRAM-SHA-256, SCRAM-SHA-512, OAUTHBEARER.", 0) \
     DECLARE(String, kafka_sasl_username, "", "SASL username for use with the PLAIN and SASL-SCRAM-.. mechanisms", 0) \
     DECLARE(String, kafka_sasl_password, "", "SASL password for use with the PLAIN and SASL-SCRAM-.. mechanisms", 0) \
+    DECLARE(KafkaSSLEndpointIdentificationAlgorithm, kafka_ssl_endpoint_identification_algorithm, KafkaSSLEndpointIdentificationAlgorithm::NONE, "SSL endpoint identification algorithm. Possible values: none, https.", 0) \
+    DECLARE(String, kafka_ssl_ca_location, "", "CA certificate file path for SSL/TLS authentication.", 0) \
+    DECLARE(String, kafka_ssl_certificate_location, "", "Client certificate file path for SSL/TLS authentication.", 0) \
+    DECLARE(String, kafka_ssl_key_location, "", "Client private key file path for SSL/TLS authentication.", 0) \
 
 #define OBSOLETE_KAFKA_SETTINGS(M, ALIAS) \
     MAKE_OBSOLETE(M, Char, kafka_row_delimiter, '\0') \
