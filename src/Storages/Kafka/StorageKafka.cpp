4b3b92e2cd3d9af3bb460c052839ffd436378fbd
Tilman Moeller <tilman.moeller@aiven.io>
2025-12-23 14:53:17 +0100

    Add extra settings to Kafka Table Engine
    These settings can be tweaked to help improve throughput when writing to
    Kafka using the Kafka table engine. The settings are configurable per
    table, allowing fine-grained control over producer behavior.
    
    New producer settings added:
    - kafka_producer_batch_size: Maximum size of a batch in bytes
    - kafka_producer_batch_num_messages: Maximum number of messages in a batch
    - kafka_producer_compression_codec: Compression codec (none, gzip, snappy, lz4, zstd)
    - kafka_producer_compression_level: Compression level (-1 for default)
    - kafka_producer_linger_ms: Delay to wait for messages to form batches
    - kafka_producer_queue_buffering_max_messages: Max messages in producer queue
    - kafka_producer_queue_buffering_max_kbytes: Max KB in producer queue
    - kafka_producer_request_required_acks: Required acknowledgments (-1, 0, or 1)
    
    All settings default to 0 or -1, which means librdkafka will use its default
    values. This maintains backward compatibility with existing configurations.

--- src/Storages/Kafka/StorageKafka.cpp
+++ src/Storages/Kafka/StorageKafka.cpp
@@ -101,6 +101,14 @@ namespace KafkaSetting
     extern const KafkaSettingsBool kafka_thread_per_consumer;
     extern const KafkaSettingsString kafka_topic_list;
     extern const KafkaSettingsString kafka_format_avro_schema_registry_url;
+    extern const KafkaSettingsKafkaCompressionCodec kafka_producer_compression_codec;
+    extern const KafkaSettingsInt64 kafka_producer_compression_level;
+    extern const KafkaSettingsUInt64 kafka_producer_linger_ms;
+    extern const KafkaSettingsUInt64 kafka_producer_queue_buffering_max_messages;
+    extern const KafkaSettingsUInt64 kafka_producer_batch_size;
+    extern const KafkaSettingsUInt64 kafka_producer_batch_num_messages;
+    extern const KafkaSettingsInt64 kafka_producer_request_required_acks;
+    extern const KafkaSettingsUInt64 kafka_producer_queue_buffering_max_kbytes;
 }
 
 namespace ErrorCodes
@@ -505,7 +513,15 @@ cppkafka::Configuration StorageKafka::getProducerConfiguration()
          (*kafka_settings)[KafkaSetting::kafka_ssl_certificate_location].value,
          (*kafka_settings)[KafkaSetting::kafka_ssl_key_location].value},
         brokers,
-        client_id};
+        client_id,
+        (*kafka_settings)[KafkaSetting::kafka_producer_batch_size].value,
+        (*kafka_settings)[KafkaSetting::kafka_producer_batch_num_messages].value,
+        SettingFieldKafkaCompressionCodecTraits::toString((*kafka_settings)[KafkaSetting::kafka_producer_compression_codec].value),
+        (*kafka_settings)[KafkaSetting::kafka_producer_compression_level].value,
+        (*kafka_settings)[KafkaSetting::kafka_producer_linger_ms].value,
+        (*kafka_settings)[KafkaSetting::kafka_producer_queue_buffering_max_messages].value,
+        (*kafka_settings)[KafkaSetting::kafka_producer_queue_buffering_max_kbytes].value,
+        (*kafka_settings)[KafkaSetting::kafka_producer_request_required_acks].value};
     return KafkaConfigLoader::getProducerConfiguration(*this, params);
 }
 
