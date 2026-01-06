25221badc43774ae1ce7d54cb8366b5aea12e087
Tilman Moeller <tilman.moeller@aiven.io>
2026-01-06 15:03:50 +0100

    Add early fetch pool When adding a replica to an existing cluster, the replica will add many GET_PART tasks to its replication queue. These tasks are in charge of downloading the data that existed before the creation of the replica.
    Meanwhile, if the cluster is still receiving updates, new GET_PART
    tasks will be created to add the extra data.
    
    These new parts are usually small, and in a cluster where the inserts
    are a bit too frequent, we can have a lot of them.
    
    However, there is a limit to the number of GET_PART tasks that a
    replica will simultaneously execute (by default 8).
    
    The early GET_PART tasks, for existing data, are often very large and
    can take a lot of time to execute.
    
    If there are more existing parts than the maximum number of
    simultanenous downloads, then the early parts will take all slots in
    the pool for download tasks and the small additional GET_PART tasks
    will have to wait for the very large early parts to be downloaded.
    
    The accumulation of many small GET_PART tasks then becomes an issue
    for ZooKeeper, each of these tasks is a znode and it's quite easy to
    end up with >100k pending tasks for a single table.
    
    We cannot simply increase the download pool size. A large cluster
    will always have more existing large parts than the pool size, unless
    we configure the pool with an unreasonably large size.
    
    However, we can split the pool in two parts: one pool for the
    existing parts that are added early to sync the replica, and another
    pool for the normal GET_PART tasks that happen because of normal
    inserts.
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Storages/MergeTree/MergeTreeSettings.cpp
+++ src/Storages/MergeTree/MergeTreeSettings.cpp
@@ -1664,6 +1664,10 @@ namespace ErrorCodes
     DECLARE(Bool, allow_remote_fs_zero_copy_replication, false, R"(
     Don't use this setting in production, because it is not ready.
     )", EXPERIMENTAL) \
+    DECLARE(Bool, use_early_fetch_pool, true, R"(
+    Use separate thread pool for early fetches (initial sync) to prevent blocking normal replication.
+    When enabled, fetches with empty source_replica use the early fetches pool instead of the normal fetches pool.
+    )", 0) \
     DECLARE(String, remote_fs_zero_copy_zookeeper_path, "/clickhouse/zero_copy", R"(
     ZooKeeper path for zero-copy table-independent info.
     )", EXPERIMENTAL) \
