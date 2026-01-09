e3ce7695e5ab021b047914a94704ff4ade9ee87f
Tilman Moeller <tilman.moeller@aiven.io>
2026-01-09 16:41:07 +0100

    Include metadata_version.txt when freezing
    The metadata_version.txt file was added to each part folder to help with
    concurrency issues when mutating the table schema, but it was not included
    in the frozen files when creating backups.
    
    We need to include this file in the frozen shadow/ folder because we use
    these files to know what to backup. When using object storage, this file is
    actually a pointer to the file in object storage. We use these pointers to
    know which files are still referenced by ClickHouse and should not be deleted.
    
    By omitting this file, we would not know about it, and delete the
    metadata_version.txt file in object storage. This was causing latent issues:
    ClickHouse doesn't immediately notice the missing file, but instead complains
    loudly when adding a new replica - the new replica tries to download this
    file when syncing from existing replicas.
    
    The fix sets keep_metadata_version = true in ClonePartParams when freezing
    parts, ensuring the metadata_version.txt file is preserved in frozen backups.
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -8874,6 +8874,7 @@ PartitionCommandsResultInfo MergeTreeData::freezePartitionsByMatcher(
 
         IDataPartStorage::ClonePartParams params
         {
+            .keep_metadata_version = true,
             .make_source_readonly = true
         };
 
