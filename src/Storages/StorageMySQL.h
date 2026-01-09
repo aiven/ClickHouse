49ff1731e11676ce4fee172b4276f2c27113f6fc
Tilman Moeller <tilman.moeller@aiven.io>
2026-01-09 15:45:18 +0100

    Add named_collection column to system.tables
    This patch adds a new `named_collection` column to the `system.tables` system
    table, which displays the name of the named collection (if any) that was used
    to create each table. This enables users to track which named collections are
    associated with their tables, particularly useful for external storage engines
    like MySQL, PostgreSQL, S3, and Azure Blob Storage.
    
    Co-authored-by: Aliaksei Khatskevich <alex.khatskevich@aiven.io>

--- src/Storages/StorageMySQL.h
+++ src/Storages/StorageMySQL.h
@@ -36,7 +36,8 @@ public:
         const ConstraintsDescription & constraints_,
         const String & comment,
         ContextPtr context_,
-        const MySQLSettings & mysql_settings_);
+        const MySQLSettings & mysql_settings_,
+        std::optional<String> named_collection_ = std::nullopt);
 
     std::string getName() const override { return "MySQL"; }
 
@@ -73,6 +74,7 @@ public:
 
         Addresses addresses; /// Failover replicas.
         String addresses_expr;
+        std::optional<String> named_collection;
     };
 
     static Configuration getConfiguration(ASTs engine_args, ContextPtr context_, MySQLSettings & storage_settings);
