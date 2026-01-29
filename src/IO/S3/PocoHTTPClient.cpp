71267bdc901e79ceea86bad12e84648bb2b1ded9
Aliaksei Khatskevich <alex.khatskevich@aiven.io>
2026-01-29 21:45:24 +0100

    Prohibit redirects from https to http
    Redirects from https to http might create attack opportunities.
    Both the client and server sides should prohibit this behavior.

--- src/IO/S3/PocoHTTPClient.cpp
+++ src/IO/S3/PocoHTTPClient.cpp
@@ -104,6 +104,7 @@ namespace DB::ErrorCodes
 {
     extern const int NOT_IMPLEMENTED;
     extern const int TOO_MANY_REDIRECTS;
+    extern const int UNACCEPTABLE_URL;
 }
 
 namespace DB::S3
@@ -606,7 +607,18 @@ void PocoHTTPClient::makeRequestInternalImpl(
             if (poco_response.getStatus() == Poco::Net::HTTPResponse::HTTP_TEMPORARY_REDIRECT)
             {
                 auto location = poco_response.get("location");
-                remote_host_filter.checkURL(Poco::URI(location));
+                Poco::URI location_uri(location);
+                remote_host_filter.checkURL(location_uri);
+
+                /// Prevent SSRF attacks by disallowing scheme downgrades (HTTPS -> HTTP).
+                Poco::URI initial_uri(request.GetUri().GetURIString());
+                if (initial_uri.getScheme() == "https" && location_uri.getScheme() == "http")
+                    throw Exception(
+                        ErrorCodes::UNACCEPTABLE_URL,
+                        "Redirect from HTTPS to HTTP is not allowed for security reasons. "
+                        "Initial URL: {}, redirect URL: {}.",
+                        initial_uri.toString(), location);
+
                 uri = location;
                 if (enable_s3_requests_logging)
                     LOG_TEST(log, "Redirecting request to new location: {}", location);
