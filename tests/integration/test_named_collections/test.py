6d776b2dddfbe29cae6dc7daa3d8feea4555a635
Joe Lynch <joelynch112@gmail.com>
2026-07-13 15:44:30 +0200

    Add test for ALTER NAMED COLLECTION propagation to replica via Keeper
    Verifies that in-place ALTER of a named collection stored in Keeper
    propagates to the other replica in both directions.

--- tests/integration/test_named_collections/test.py
+++ tests/integration/test_named_collections/test.py
@@ -8,6 +8,7 @@ import uuid
 
 from helpers.client import QueryRuntimeException
 from helpers.cluster import ClickHouseCluster
+from helpers.test_tools import assert_eq_with_retry
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 NAMED_COLLECTIONS_CONFIG = os.path.join(
@@ -844,6 +845,59 @@ def test_keeper_storage(cluster):
 
     check_dropped(node1)
     check_dropped(node2)
+
+
+def test_keeper_storage_alter_propagates_to_replica(cluster):
+    node1 = cluster.instances["node_with_keeper"]
+    node2 = cluster.instances["node_with_keeper_2"]
+    zk = cluster.get_kazoo_client("zoo1")
+
+    collection = "alter_propagation_coll"
+    node1.query(f"DROP NAMED COLLECTION IF EXISTS {collection}")
+
+    try:
+        node1.query(f"CREATE NAMED COLLECTION {collection} AS key1='initial'")
+
+        # Both replicas must first see the created collection.
+        for node in (node1, node2):
+            assert_eq_with_retry(
+                node,
+                f"SELECT collection['key1'] FROM system.named_collections WHERE name = '{collection}'",
+                "initial",
+            )
+
+        zk.sync(ZK_PATH)
+        children_before = sorted(zk.get_children(ZK_PATH))
+
+        node1.query(
+            f"ALTER NAMED COLLECTION {collection} SET key1='updated', key2='added'"
+        )
+
+        assert_eq_with_retry(
+            node2,
+            f"SELECT collection['key1'], collection['key2'] FROM system.named_collections WHERE name = '{collection}'",
+            "updated\tadded",
+        )
+
+        node2.query(
+            f"ALTER NAMED COLLECTION {collection} SET key1='final' DELETE key2"
+        )
+        assert_eq_with_retry(
+            node1,
+            f"SELECT collection['key1'], mapContains(collection, 'key2') FROM system.named_collections WHERE name = '{collection}'",
+            "final\t0",
+        )
+
+        zk.sync(ZK_PATH)
+        assert children_before == sorted(zk.get_children(ZK_PATH))
+    finally:
+        node1.query(f"DROP NAMED COLLECTION IF EXISTS {collection}")
+        for node in (node1, node2):
+            assert_eq_with_retry(
+                node,
+                f"SELECT count() FROM system.named_collections WHERE name = '{collection}'",
+                "0",
+            )
 
 
 @pytest.mark.parametrize(
