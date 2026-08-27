ff1bbfc4132c6c23aa7fbbbf3a3fe92be433790b
Aliaksei Khatskevich <alex.khatskevich@aiven.io>
2026-08-27 11:02:08 +0200

    Backport #106946 to 26.3: cluster function parallelism
    Clamp customer-controlled cluster-function stream counts to available CPU
    cores before URL, file, and object-storage readers capture the value.
    
    https://github.com/ClickHouse/ClickHouse/pull/106946

--- src/Storages/ObjectStorage/StorageObjectStorage.cpp
+++ src/Storages/ObjectStorage/StorageObjectStorage.cpp
@@ -581,7 +581,8 @@ void StorageObjectStorage::read(
     }
 
     if (distributed_processing && local_context->getSettingsRef()[Setting::max_streams_for_files_processing_in_cluster_functions])
-        num_streams = local_context->getSettingsRef()[Setting::max_streams_for_files_processing_in_cluster_functions];
+        num_streams = clampClusterFunctionNumStreams(
+            local_context->getSettingsRef()[Setting::max_streams_for_files_processing_in_cluster_functions]);
 
     /// For data lake we did update in getExternalDynamicMetadata.
     if (!is_table_function && !configuration->isDataLakeConfiguration())
